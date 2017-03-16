fe033148364637185b09c70a7103c3f32732acf5
jc350949 <cameron.gauci@my.jcu.edu.au>
2017-03-17 08:22:16 +1000

    First code

--- oddName.py
+++ oddName.py
@@ -1,3 +1,20 @@
 """
 Cameron
 """
+count = 1
+
+name = input("What is your Name?")
+
+
+
+if len(name) > 1:
+    for char in name:
+            range(0,len(name),2)
+            count += 1
+            print(char:2)
+
+else:
+    print("Not a valid name")
+
+
+
